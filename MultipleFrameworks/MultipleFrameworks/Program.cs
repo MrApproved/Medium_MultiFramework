982b9e3e06e3cc66cd860b15bed97f473f7c7adf
sdsad <bengwasdasdsaatts@outlook.com>
2020-05-11 20:21:00 +0100

    Init

---
+++ MultipleFrameworks/MultipleFrameworks/Program.cs
@@ -0,0 +1,20 @@
+﻿using StandardMultipleLibrary;
+using System;
+
+namespace MultipleFrameworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = new MultiFrameworks();
+
+            Console.WriteLine($"Platform: {library.Platform}");
+            Console.WriteLine($"Version: {Environment.Version}");
+            Console.WriteLine("Press return to exit...");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
